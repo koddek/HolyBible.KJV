bdd871c6760bc2075afffed6ecfafee55cb6a051
codedek <codedek@outlook.com>
2018-05-20 02:37:41 -0400

    added: xml comments. added: 2 overloads for Find, can now find all passages of a book of the bible and all passages of a chapter of a book of the bible.

--- src/HolyBible.KJV/Bible.cs
+++ src/HolyBible.KJV/Bible.cs
@@ -10,9 +10,39 @@ namespace HolyBible.KJV
 
     public List<Passage> Passages { get; } = new List<Passage>();
 
+    /// <summary>
+    /// Find all passages of a book of the bible.
+    /// </summary>
+    /// <param name="book">The book.</param>
+    /// <returns></returns>
+    public IEnumerable<Passage> Find(Book book) =>
+      Passages.FindAll(p => p.Book == book);
+
+    /// <summary>
+    /// Find all passages of a chapter of a book of the bible.
+    /// </summary>
+    /// <param name="book">The book.</param>
+    /// <param name="chapter">The chapter.</param>
+    /// <returns></returns>
+    public IEnumerable<Passage> Find(Book book, int chapter) =>
+      Passages.FindAll(p => p.Book == book && p.Chapter == chapter);
+
+    /// <summary>
+    /// Find a <see cref="Passage"/> of scripture.
+    /// </summary>
+    /// <param name="book">The book of the bible.</param>
+    /// <param name="chapter">The chapter.</param>
+    /// <param name="verse">The verse</param>
+    /// <returns></returns>
     public Passage Find(Book book, int chapter, int verse) =>
       Passages.Find(p => p.Book == book && p.Chapter == chapter && p.Verse == verse);
 
+    /// <summary>
+    /// Find all passages in the bible that contains a word or phrase, case sensitive or not. 
+    /// </summary>
+    /// <param name="filter">The word or phrase</param>
+    /// <param name="ignoreCase">Ignore the case or not?</param>
+    /// <returns></returns>
     public IEnumerable<Passage> Find(string filter, bool ignoreCase = true)
     {
       foreach (var passage in Passages)

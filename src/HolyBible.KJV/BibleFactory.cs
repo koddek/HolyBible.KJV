f0fd16d28077bece59d40d78a21271f82d674e28
codedek <codedek@outlook.com>
2018-05-20 02:38:47 -0400

    added a async method to load the bible from resources.

--- src/HolyBible.KJV/BibleFactory.cs
+++ src/HolyBible.KJV/BibleFactory.cs
@@ -1,5 +1,6 @@
 ﻿using HolyBible.KJV.Properties;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace HolyBible.KJV
 {
@@ -12,7 +13,17 @@ namespace HolyBible.KJV
 
       // From byte array to string
       var s = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
-      return JsonConvert.DeserializeObject<Bible>(s); 
+      return JsonConvert.DeserializeObject<Bible>(s);
+    }
+
+    public static async Task<Bible> LoadBibleAsync()
+    {
+      // From string to byte array
+      var buffer = Resources.kjvbible;
+
+      // From byte array to string
+      var s = await Task.Run(() => System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+      return await Task.Run(() => JsonConvert.DeserializeObject<Bible>(s));
     }
   }
 }
